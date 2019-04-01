38e8ba0392801664241d179a5b513385aa20c244
winniepopu <winniepopu@gmail.com>
2019-04-01 21:37:25 +0800

    finished

--- dist/student.js
+++ dist/student.js
@@ -1,143 +1,150 @@
-
+var list_count = 0;
 $(document).ready(function() {
+    $('#btn-list').click((event) => {
+        event.preventDefault()
+        if (list_count % 2 == 0)
+            loadData();
+        else
+            cancelData();
+        list_count++;
+    })
 
-var list_count=0;
-  $('#btn-list').click((event) => {
-    event.preventDefault()
-    if (list_count%2==0)
-      loadData();
-    else
-      cancelData();
-    list_count++;  
-  })
+    $('#btn-search').click((event) => {
+        event.preventDefault()
+        const ID = $("#ajax-search input[name=studentID]").val();
+        findStudentID(ID);
+    })
 
+    $('#btn-add').click((event) => {
+        event.preventDefault()
+        const ID = $("#ajax-add input[name=studentID]").val();
+        const Stname = $("#ajax-add input[name=studentName]").val();
 
-  $('#btn-search').click((event) => {
-    event.preventDefault()
-      const ID = $("#ajax-search input[name=studentID]").val();
-      findStudentID(ID);
- 
-  
-  })
+        addStudentID(ID, Stname);
+    })
 
-  $('#btn-add').click((event) => {
-    event.preventDefault()
-      const ID = $("#ajax-add input[name=studentID]").val();
-      
-      const Stname = $("#ajax-add input[name=studentName]").val();
-    
-
-      console.log(ID);  
-      addStudentID(ID,Stname);
- 
-
-  })
-
+    $('#btn-del').click((event) => {
+        event.preventDefault()
+        const ID = $("#ajax-del input[name=studentID]").val();
+        delStudentID(ID);
+    })
 });
 
 
-const dataURL = "students.json";
+// const dataURL = "students.json";
 
-function loadData(){    
-$.ajax({
-    url:dataURL,
-      type: "GET",
+function loadData() {
+    $.ajax({
+        url: 'readJSON',
+        type: "POST",
         dataType: "json",
-          success: function(Jdata) {
-            
-       //     if (mode==1)
-                generateDataHtml(Jdata);
-         //   else if (mode==2)
-           //     findStudentID(Jdata);
-
-                  console.log(Jdata);}
-                , error: function() { alert("ERROR!!!");
-                    }
-});
+        success: function(Jdata) {
+            generateDataHtml(Jdata);
+        },
+        error: function() {
+            alert("ERROR!!!");
+        }
+    });
 }
 
-    function generateDataHtml(data) {
-         var html_element=''
-            for (key in data){
-               const element = `<h3>${key} : ${data[key]}</h3>`;
-              html_element+=element;
-       }
-            $("#ajax-output").html(html_element);
-                              }
-
-    function cancelData(){
-          const element='' ;
-            $("#ajax-output").html(element);
-  
+function generateDataHtml(data) {
+    var html_element = ''
+    for (key in data) {
+        const element = `<h3>"${key}": "${data[key]}"</h3>`;
+        html_element += element;
     }
+    $("#ajax-output").html(html_element);
+}
 
+function cancelData() {
+    const element = '';
+    $("#ajax-output").html(element);
+}
 
-    function findStudentID(ID) {
-         
-      $.ajax({
-          url:dataURL,
-            type: "GET",
-              dataType: "json",
-                success: function(Jdata) {
-                  findName(Jdata,ID);                  
-                        console.log(Jdata);}
-                      , error: function() { alert("ERROR!!!");
-                          }
-      });
-      
-    }
-
-
-
-
-
-    function findName(data,ID)
-    {
-      const element = `<h2>Hello, ${data[ID]}</h2>`;
-
-            $("#ajax-specific").html(element);
-    }     
-
-
-    function addStudentID(ID,Stname){
-      const inputData = {};
-      inputData['ID']=ID;
-      inputData['name']=Stname;
-
-
-      console.log(inputData);
-    // inputData[ID] = Stname;
-     // const inputData={ID:Stname};
-     // console.log(inputData);
-      
-      const DD =  JSON.stringify(inputData);
-      console.log(DD)
-
-      console.log('URL:',dataURL)
+function findStudentID(ID) {
+    if (ID == '')
+        alert("The columns can't be null, please enter the value");
+    else {
         $.ajax({
-              url:'saveJSON',
-              type: "POST",
-              data: inputData,
-              datatype: "json",  
-              success: function(data) {
-                  console.log('success');}
-                      , 
-              
-              error: function(data,sta,type) { alert("ERROR!!!")
-console.log(sta)
-  console.log(type)
-              console.log(data)          
-              console.log(data.status)          
-                        ;
- 
-                  console.log('error');
-                      
-                  }
-             
-                      
-  
-                      })
+            url: 'readJSON',
+            type: "POST",
+            dataType: "json",
+            success: function(Jdata) {
+                generateNameHtml(Jdata, ID);
+            },
+            error: function() {
+                alert("ERROR!!!");
+            }
+        });
     }
-    
+}
 
 
+function generateNameHtml(data, ID) {
+    if (!data[ID]) {
+        alert(`There's no student's ID is ${ID} , please try again.`);
+    } else {
+        const element = `</br><h1>Hello, ${data[ID]}</h1>`;
+        $("#ajax-specific").html(element);
+    }
+
+}
+
+
+function addStudentID(ID, Stname) {
+
+    if (ID == '' || Stname == '')
+        alert("The columns can't be null, please enter the value");
+    else {
+        const inputData = {};
+        inputData['ID'] = ID;
+        inputData['name'] = Stname;
+
+
+        $.ajax({
+            url: 'saveJSON',
+            type: "POST",
+            data: inputData,
+            datatype: "json",
+            success: function(data) {
+
+                if (data == 'existed') { alert(`The student ID :${ID} has existed, please check whether it's correct`); } else {
+                    alert('Success add!');
+                    if (list_count % 2 != 0) { loadData(); }
+                }
+            },
+
+            error: function(data, sta, type) {
+                alert("ERROR!!!")
+            }
+        })
+
+    }
+}
+
+function delStudentID(ID) {
+    const inputData = {};
+
+    if (ID == '')
+        alert("The columns can't be null, please enter the value");
+    else {
+        inputData['ID'] = ID;
+
+        $.ajax({
+            url: 'delJSON',
+            type: "POST",
+            data: inputData,
+            datatype: "json",
+            success: function(data) {
+                if (data == 'notExisted') { alert(`The student ID :${ID} has not existed, Please enter the right ID.`); } else {
+                    alert('Success delete!');
+                    if (list_count % 2 != 0) { loadData(); }
+                }
+            },
+            error: function(data, sta, type) {
+                alert("ERROR!!!")
+            }
+        })
+
+    }
+}
--- ser.js
+++ ser.js
@@ -4,42 +4,39 @@
 // Step 1 code goes here
 
 const express = require('express');
-const fs=require('fs');
+const fs = require('fs');
 // create an express, aka web server, instance
 // 建立一個 express (也就是網頁伺服器)實體
 const app = express()
-  const port = 11255
+const port = 11255
 
 
 
-  app.get('/step1', (req, res) => {
-
-
-
-    res.send('<h2>hello world</h1>')
-  })
+// app.get('/step1', (req, res) => {
+//     res.send('<h2>hello world</h1>')
+// })
 
 
 
 app.listen(port, () => {
-  console.log(`listening on port: ${port}`)
+    console.log(`listening on port: ${port}`)
 })
 
 // Step 4 code goes here
-let nRequests = 0
-app.get('/step4', (req, res) => {
-  res.send(`this is request #${++nRequests}`)
-})
+// let nRequests = 0
+// app.get('/step4', (req, res) => {
+//     res.send(`this is request #${++nRequests}`)
+// })
 
 // Step 3 code goes here
 app.use(express.static(`${__dirname}/dist`))
 
-  // Step 5.1 code goes here
-  // serve `/get` url
+// Step 5.1 code goes here
+// serve `/get` url
 
-  app.get('/Step5', function(req, res) {
-    res.send(`<h1>Hello, ${req.query.fname} ${req.query.lname}</h1>`)
-  })
+// app.get('/Step5', function(req, res) {
+//     res.send(`<h1>Hello, ${req.query.fname} ${req.query.lname}</h1>`)
+// })
 
 
 const bodyParser = require('body-parser')
@@ -49,47 +46,61 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-  //app.post('/step7', (req, res) => {
-  // `bady-parser` stores parsed data in `req.body`
-  // `bady-parser` 將解析好的資料存放在 `req.body`
-  //res.send(`<h1>Hello, ${req.body.fname} ${req.body.lname}</h1>`)
-  //})
+//app.post('/step7', (req, res) => {
+// `bady-parser` stores parsed data in `req.body`
+// `bady-parser` 將解析好的資料存放在 `req.body`
+//res.send(`<h1>Hello, ${req.body.fname} ${req.body.lname}</h1>`)
+//})
 
-  //app.post('/add', function(req, res){
-  //    var obj = {};
-  //      console.log('body: ' + JSON.stringify(req.body));
-  //  res.send(req.body);
-  //});
-  var student;
-  fs.readFile('dist/students.json',(err,data) => {
+//app.post('/add', function(req, res){
+//    var obj = {};
+//      console.log('body: ' + JSON.stringify(req.body));
+//  res.send(req.body);
+//});
+var student;
+fs.readFile('dist/students.json', (err, data) => {
     console.log('in read file');
     if (err) throw err;
     student = JSON.parse(data);
     console.log(student);
-  });
+});
 
 
-
-app.post('/saveJSON',function(req, res){
-  // 物件轉換為字串
-  //
-  console.log(req.body);
-  console.log(req.body['ID']);
-  console.log(req.body['name']);
-  
-  student[req.body['ID']]=req.body['name'];
-
-  let data = JSON.stringify(student);  
-  fs.writeFileSync('dist/students.json', data);    
+app.post('/readJSON', function(req, res) {
+    res.send(student)
+});
 
 
+app.post('/saveJSON', function(req, res) {
+    // console.log(student.hasOwnProperty(req.body['ID']));
 
-
-
-  console.log('read file');
+    if (student.hasOwnProperty(req.body['ID'])) {
+        res.send("existed")
+    } else {
+        student[req.body['ID']] = req.body['name'];
+        let data = JSON.stringify(student);
+        fs.writeFileSync('dist/students.json', data);
+        res.send('done');
+    }
 
 });
 
+app.post('/delJSON', function(req, res) {
+
+    if (!student.hasOwnProperty(req.body['ID'])) {
+        res.send("notExisted")
+    } else {
+        delete student[req.body['ID']];
+        let data = JSON.stringify(student);
+        fs.writeFileSync('dist/students.json', data);
+        res.send('done');
+
+    }
+
+    // delete student[req.body['ID']];
+    // let data = JSON.stringify(student);
+    // fs.writeFileSync('dist/students.json', data);
+});
 
 
 /*
